9e53ebd4a029c52d79dfc5e0d383ebd6cfe0d1c7
John Kevin Losito <48610130+johnkevinlosito@users.noreply.github.com>
2023-05-16 23:25:02 +0800

    fix build error

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -31,12 +31,13 @@ const HeroSection = () => {
       <div className="mx-auto max-w-7xl px-6 lg:py-28 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
           <h1 className="gradient-text max-w-2xl text-4xl font-bold tracking-tight sm:text-6xl lg:col-span-2 xl:col-auto">
-            Hey, I'm John Kevin 👋
+            Hey, I&apos;m John Kevin 👋
           </h1>
           <div className="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
             <p className="text-lg lg:text-2xl leading-8">
-              I'm a passionate web and mobile developer. Naturally curious, and
-              always seeking new knowledge and actively developing new skills.
+              I&apos;m a passionate web and mobile developer. Naturally curious,
+              and always seeking new knowledge and actively developing new
+              skills.
             </p>
             <div className="mt-10 flex items-center gap-x-2">
               {socialLinks.map((social) => (
