4bbc4efdf2093f60818c1a413d008ef60168ba42
John Kevin Losito <48610130+johnkevinlosito@users.noreply.github.com>
2023-05-23 14:23:38 +0800

    update error and not found pages

--- src/app/(main)/error.tsx
+++ src/app/(main)/error.tsx
@@ -1,6 +1,8 @@
 "use client"; // Error components must be Client Components
 
 import Container from "@/components/Container";
+import Image from "next/image";
+import Link from "next/link";
 import { useEffect } from "react";
 
 export default function Error({
@@ -16,13 +18,28 @@ export default function Error({
   }, [error]);
 
   return (
-    <Container>
+    <Container className="flex flex-col items-center gap-4">
       <h1 className="gradient-text text-3xl md:text-4xl lg:text-5xl mt-8 pb-4">
         Something went wrong
       </h1>
+      <Image
+        src="/assets/bug-fixing-animate.svg"
+        alt=""
+        loading="eager"
+        height={300}
+        width={450}
+        className="max-w-full rounded-2xl object-cover lg:max-w-none aspect-square"
+      />
       <p className="text-lg">
         Please try again later, or check that you are on the correct link.
       </p>
+
+      <Link
+        href="/"
+        className="py-2 px-4 font-bold border hover:gradient-text rounded"
+      >
+        HOME
+      </Link>
     </Container>
   );
 }
--- src/app/(main)/not-found.tsx
+++ src/app/(main)/not-found.tsx
@@ -1,13 +1,32 @@
 import Container from "@/components/Container";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const NotFound = () => {
   return (
-    <Container>
+    <Container className="flex flex-col items-center gap-4">
       <h1 className="gradient-text text-3xl md:text-4xl lg:text-5xl mt-8 pb-4">
         Page not found
       </h1>
-      <p className="text-lg">Could not find requested resource.</p>
+      <Image
+        src="/assets/404-error-animate.svg"
+        alt=""
+        loading="eager"
+        height={300}
+        width={450}
+        className="max-w-full rounded-2xl object-cover lg:max-w-none aspect-square"
+      />
+      <p className="text-lg">
+        Sorry, we could not find the requested resource.
+      </p>
+
+      <Link
+        href="/"
+        className="py-2 px-4 font-bold border hover:gradient-text rounded"
+      >
+        HOME
+      </Link>
     </Container>
   );
 };
