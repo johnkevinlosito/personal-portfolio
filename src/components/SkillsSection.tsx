83bb91acfbb32c1c009e79457f5cbc4bfd8cd68a
John Kevin Losito <48610130+johnkevinlosito@users.noreply.github.com>
2023-05-24 14:29:28 +0800

    update skills section

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -7,22 +7,30 @@ import Javascript from "@root/public/assets/JavaScript.svg";
 import Nextjs from "@root/public/assets/Nextjs.svg";
 import ReactJs from "@root/public/assets/React.svg";
 import Tailwind from "@root/public/assets/TailwindCSS.svg";
+import CSS3 from "@root/public/assets/CSS3.svg";
+import HTML5 from "@root/public/assets/HTML5.svg";
+import Docker from "@root/public/assets/Docker.svg";
+import GitLab from "@root/public/assets/GitLab.svg";
 
 const SkillsSection = () => {
   return (
     <section id="tools" className="my-40">
       <Container>
-        <h2 className="gradient-text font-bold text-2xl md:text-3xl lg:text-4xl mt-14">
-          My skills and tools used
+        <h2 className="gradient-text font-bold text-2xl md:text-3xl lg:text-4xl mt-14 pb-8">
+          My technical skills and tools
         </h2>
-        <div className="flex flex-wrap items-center justify-center max-w-5xl mt-8 mx-auto gap-x-8 md:gap-x-12 gap-y-4 md:gap-y-8">
+        <div className="flex flex-wrap items-center justify-center max-w-4xl mt-8 mx-auto gap-x-8 md:gap-x-12 gap-y-4 md:gap-y-8">
+          <HTML5 className="text-[4rem]" />
+          <CSS3 className="text-[4rem]" />
           <Javascript className="text-[4rem]" />
           <ReactJs className="text-[4rem]" />
           <Nextjs className="text-[4rem] text-black dark:text-white" />
-          <Flutter className="text-[4rem]" />
-          <Tailwind className="text-[4rem]" />
-          <Aws className="text-[4rem] text-[#252F3E] dark:text-white" />
+          <Flutter className="text-[3.5rem]" />
+          <Tailwind className="text-[4.5rem]" />
+          <Aws className="text-[4.5rem] text-[#252F3E] dark:text-white" />
           <Git className="text-[4rem]" />
+          <Docker className="text-[5.5rem]" />
+          <GitLab className="text-[4rem]" />
         </div>
       </Container>
     </section>
